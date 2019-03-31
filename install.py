5b51bb03cbb6b54733a68b4bff65678b4a979b3e
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2019-03-31 19:28:45 -0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,13 @@
+#by abdala
+import os
+import time
+from sources import *
+
+print "[1] for kali"
+print "[2] for termux"
+tool = raw_input("protocol ==> ")
+   
+if tool == "1" or tool == "01":
+     kali_user()
+elif tool == "2" or tool == "02":
+     termux_user()
